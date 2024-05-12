03b71206839026ce252b8704051b7a20577d8aac
TheMalachite <eliasgheeraert@gmail.com>
2024-05-12 21:50:35 +0200

    Pacman-SAE: Création auto de label chaques joueur trouvées dans la base de données pour le menu de chargement de sauvegarde

--- ChargementPartieForm.Designer.cs
+++ ChargementPartieForm.Designer.cs
@@ -29,13 +29,7 @@
         private void InitializeComponent()
         {
             this.label_titre = new System.Windows.Forms.Label();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.button_retour_startpartie = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label_titre
@@ -48,65 +42,6 @@
             this.label_titre.TabIndex = 6;
             this.label_titre.Text = "Sauvegarde";
             this.label_titre.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.BackColor = System.Drawing.Color.DimGray;
-            this.tableLayoutPanel1.ColumnCount = 4;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.Controls.Add(this.label4, 3, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label3, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(63, 192);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(852, 47);
-            this.tableLayoutPanel1.TabIndex = 7;
-            // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(84, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(297, 15);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
-            // 
-            // label3
-            // 
-            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(510, 15);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 16);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
-            // 
-            // label4
-            // 
-            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(723, 15);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(44, 16);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "label4";
             // 
             // button_retour_startpartie
             // 
@@ -126,12 +61,10 @@
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(979, 640);
             this.Controls.Add(this.button_retour_startpartie);
-            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.label_titre);
             this.Name = "ChargementPartieForm";
             this.Text = "ChargementPartieForm";
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
+            this.Load += new System.EventHandler(this.ChargementPartieForm_Load);
             this.ResumeLayout(false);
 
         }
@@ -139,11 +72,6 @@
         #endregion
 
         private System.Windows.Forms.Label label_titre;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button_retour_startpartie;
     }
 }
--- ChargementPartieForm.cs
+++ ChargementPartieForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Pacman_SAE
 {
@@ -24,6 +25,80 @@ namespace Pacman_SAE
             Form mainForm = new MainForm();
             mainForm.FormClosed += (s, args) => this.Close();
             mainForm.Show();
+        }
+
+        private void ChargementPartieForm_Load(object sender, EventArgs e)
+        {
+            MySql.Data.MySqlClient.MySqlConnection connection;
+            string myConnectionString = $"server=10.1.139.236;user=d4;pwd=mdp;Database=based4";
+
+            string query = "SELECT nom_joueur, nbVies_joueur, score_joueur, id_monde FROM Joueur";
+            int labelYPosition = 200;
+
+            try
+            {
+                connection = new MySql.Data.MySqlClient.MySqlConnection();
+                connection.ConnectionString = myConnectionString;
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read()) // Faire une boucle pour tout les joueurs trouvées
+                {
+                    string playerName = reader.GetString("nom_joueur");
+                    int nbVies_joueur = reader.GetInt32("nbVies_joueur");
+                    int score_joueur = reader.GetInt32("score_joueur");
+                    int worldId = reader.GetInt32("id_monde");
+
+                    string formattedText = playerName;
+                    formattedText += $"Monde: {worldId}";
+                    formattedText += $"Vies: {nbVies_joueur}";
+                    formattedText += $"Score: {score_joueur}";
+
+                    Label playerNameLabel = new Label();
+                    playerNameLabel.ForeColor = Color.White;
+                    playerNameLabel.Text = playerName;
+                    playerNameLabel.Location = new Point(84, labelYPosition); // Position du nom du joueur
+                    playerNameLabel.AutoSize = true;
+                    playerNameLabel.Size = new System.Drawing.Size(44, 16);
+
+                    Label worldIdLabel = new Label();
+                    worldIdLabel.ForeColor = Color.White;
+                    worldIdLabel.Text = $"Monde: {worldId}";
+                    worldIdLabel.Location = new Point(297, labelYPosition); // Position du monde
+                    worldIdLabel.AutoSize = true;
+                    worldIdLabel.Size = new System.Drawing.Size(44, 16);
+
+                    Label livesLabel = new Label();
+                    livesLabel.ForeColor = Color.White;
+                    livesLabel.Text = $"Vies: {nbVies_joueur}";
+                    livesLabel.Location = new Point(510, labelYPosition); // Position des vies
+                    livesLabel.AutoSize = true;
+                    livesLabel.Size = new System.Drawing.Size(44, 16);
+
+                    Label scoreLabel = new Label();
+                    scoreLabel.ForeColor = Color.White;
+                    scoreLabel.Text = $"Score: {score_joueur}";
+                    scoreLabel.Location = new Point(723, labelYPosition); // Position du score
+                    scoreLabel.AutoSize = true;
+                    scoreLabel.Size = new System.Drawing.Size(44, 16);
+
+                    // Ajouts des labels sur la form
+                    this.Controls.Add(playerNameLabel);
+                    this.Controls.Add(worldIdLabel);
+                    this.Controls.Add(livesLabel);
+                    this.Controls.Add(scoreLabel);
+
+                    labelYPosition += playerNameLabel.Height + 70; // Ajuste la position Y pour le prochain joueur
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                // Affichage d'un message d'erreur lors de problème de récupération de données
+                MessageBox.Show($"Erreur pendant la récupération de données: {ex.Message}");
+            }
         }
     }
 }
