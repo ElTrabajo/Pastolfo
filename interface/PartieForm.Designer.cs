4e26894c1e4ad52c2b577c1b9ea8c58fb44e0e15
TheMalachite <eliasgheeraert@gmail.com>
2024-06-06 19:29:11 +0200

    Pacman-SAE: Suppression d'un label non utiliser

--- interface/PartieForm.Designer.cs
+++ interface/PartieForm.Designer.cs
@@ -28,23 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.labelViesJoueur = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // labelViesJoueur
-            // 
-            this.labelViesJoueur.Location = new System.Drawing.Point(65, 598);
-            this.labelViesJoueur.Name = "labelViesJoueur";
-            this.labelViesJoueur.Size = new System.Drawing.Size(100, 23);
-            this.labelViesJoueur.TabIndex = 0;
-            this.labelViesJoueur.Text = "Vies :";
             // 
             // PartieForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(979, 640);
-            this.Controls.Add(this.labelViesJoueur);
             this.Name = "PartieForm";
             this.Text = "PartieForm";
             this.Load += new System.EventHandler(this.PartieForm_Load);
@@ -53,7 +43,5 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label labelViesJoueur;
     }
 }
