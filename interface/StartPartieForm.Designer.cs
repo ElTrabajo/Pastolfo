93f291c64e352b59940fbd4a90544a5f515e62af
Loan <frnt.loan@gmail.com>
2024-06-13 09:08:12 +0200

    J'ai mis un "O"

--- interface/StartPartieForm.Designer.cs
+++ interface/StartPartieForm.Designer.cs
@@ -42,9 +42,10 @@
             // 
             this.label_titre.Font = new System.Drawing.Font("Microsoft YaHei UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label_titre.ForeColor = System.Drawing.SystemColors.Control;
-            this.label_titre.Location = new System.Drawing.Point(245, 52);
+            this.label_titre.Location = new System.Drawing.Point(184, 42);
+            this.label_titre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label_titre.Name = "label_titre";
-            this.label_titre.Size = new System.Drawing.Size(489, 107);
+            this.label_titre.Size = new System.Drawing.Size(373, 87);
             this.label_titre.TabIndex = 5;
             this.label_titre.Text = "Pac-Stolfo";
             this.label_titre.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -53,18 +54,20 @@
             // 
             this.label_nomjoueur.AutoSize = true;
             this.label_nomjoueur.ForeColor = System.Drawing.SystemColors.Control;
-            this.label_nomjoueur.Location = new System.Drawing.Point(429, 223);
+            this.label_nomjoueur.Location = new System.Drawing.Point(322, 181);
+            this.label_nomjoueur.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label_nomjoueur.Name = "label_nomjoueur";
-            this.label_nomjoueur.Size = new System.Drawing.Size(120, 16);
+            this.label_nomjoueur.Size = new System.Drawing.Size(98, 13);
             this.label_nomjoueur.TabIndex = 6;
             this.label_nomjoueur.Text = "NOM DU JOUEUR";
             // 
             // textBox_nomjoueur
             // 
             this.textBox_nomjoueur.ForeColor = System.Drawing.SystemColors.InactiveCaption;
-            this.textBox_nomjoueur.Location = new System.Drawing.Point(329, 260);
+            this.textBox_nomjoueur.Location = new System.Drawing.Point(247, 211);
+            this.textBox_nomjoueur.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox_nomjoueur.Name = "textBox_nomjoueur";
-            this.textBox_nomjoueur.Size = new System.Drawing.Size(321, 22);
+            this.textBox_nomjoueur.Size = new System.Drawing.Size(242, 20);
             this.textBox_nomjoueur.TabIndex = 7;
             this.textBox_nomjoueur.Text = "Veuillez saisir votre pseudo ici";
             this.textBox_nomjoueur.Enter += new System.EventHandler(this.textBox_nomjoueur_Enter);
@@ -74,17 +77,19 @@
             // 
             this.label_gamemode.AutoSize = true;
             this.label_gamemode.ForeColor = System.Drawing.SystemColors.Control;
-            this.label_gamemode.Location = new System.Drawing.Point(440, 377);
+            this.label_gamemode.Location = new System.Drawing.Point(330, 306);
+            this.label_gamemode.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label_gamemode.Name = "label_gamemode";
-            this.label_gamemode.Size = new System.Drawing.Size(98, 16);
+            this.label_gamemode.Size = new System.Drawing.Size(80, 13);
             this.label_gamemode.TabIndex = 8;
             this.label_gamemode.Text = "MODE DE JEU";
             // 
             // button_startgame
             // 
-            this.button_startgame.Location = new System.Drawing.Point(397, 567);
+            this.button_startgame.Location = new System.Drawing.Point(298, 461);
+            this.button_startgame.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button_startgame.Name = "button_startgame";
-            this.button_startgame.Size = new System.Drawing.Size(184, 32);
+            this.button_startgame.Size = new System.Drawing.Size(138, 26);
             this.button_startgame.TabIndex = 11;
             this.button_startgame.Text = "Commencer";
             this.button_startgame.UseVisualStyleBackColor = true;
@@ -93,9 +98,10 @@
             // radioButton_classique
             // 
             this.radioButton_classique.Appearance = System.Windows.Forms.Appearance.Button;
-            this.radioButton_classique.Location = new System.Drawing.Point(362, 411);
+            this.radioButton_classique.Location = new System.Drawing.Point(272, 334);
+            this.radioButton_classique.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.radioButton_classique.Name = "radioButton_classique";
-            this.radioButton_classique.Size = new System.Drawing.Size(127, 42);
+            this.radioButton_classique.Size = new System.Drawing.Size(95, 34);
             this.radioButton_classique.TabIndex = 12;
             this.radioButton_classique.TabStop = true;
             this.radioButton_classique.Text = "CLASSIQUE";
@@ -105,9 +111,10 @@
             // radioButton_survie
             // 
             this.radioButton_survie.Appearance = System.Windows.Forms.Appearance.Button;
-            this.radioButton_survie.Location = new System.Drawing.Point(489, 411);
+            this.radioButton_survie.Location = new System.Drawing.Point(367, 334);
+            this.radioButton_survie.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.radioButton_survie.Name = "radioButton_survie";
-            this.radioButton_survie.Size = new System.Drawing.Size(127, 42);
+            this.radioButton_survie.Size = new System.Drawing.Size(95, 34);
             this.radioButton_survie.TabIndex = 13;
             this.radioButton_survie.TabStop = true;
             this.radioButton_survie.Text = "SURVIE";
@@ -117,9 +124,10 @@
             // button_retour_startpartie
             // 
             this.button_retour_startpartie.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_retour_startpartie.Location = new System.Drawing.Point(867, 572);
+            this.button_retour_startpartie.Location = new System.Drawing.Point(650, 465);
+            this.button_retour_startpartie.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button_retour_startpartie.Name = "button_retour_startpartie";
-            this.button_retour_startpartie.Size = new System.Drawing.Size(75, 32);
+            this.button_retour_startpartie.Size = new System.Drawing.Size(56, 26);
             this.button_retour_startpartie.TabIndex = 14;
             this.button_retour_startpartie.Text = "Retour";
             this.button_retour_startpartie.UseVisualStyleBackColor = true;
@@ -127,10 +135,10 @@
             // 
             // StartPartieForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(979, 640);
+            this.ClientSize = new System.Drawing.Size(734, 520);
             this.Controls.Add(this.button_retour_startpartie);
             this.Controls.Add(this.radioButton_survie);
             this.Controls.Add(this.radioButton_classique);
@@ -139,6 +147,7 @@
             this.Controls.Add(this.textBox_nomjoueur);
             this.Controls.Add(this.label_nomjoueur);
             this.Controls.Add(this.label_titre);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "StartPartieForm";
             this.Text = "Pacman";
             this.ResumeLayout(false);
